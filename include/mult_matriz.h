a80a96cb98ee9572802d91af47185692f552a430
Joao Emmanuel <joaoei@localhost.localdomain>
2018-03-24 19:49:04 -0300

    Leitura de arquivos

--- include/mult_matriz.h
+++ include/mult_matriz.h
@@ -1,5 +1,8 @@
 using namespace std;
 #include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
 
 void mult_matriz_seq(vector<vector<int>> matrizA, vector<vector<int>> matrizB, vector<vector<int>> &matrizC, int tam) {
 	//Programa...
@@ -9,10 +12,33 @@ void mult_matriz_conc(vector<vector<int>> matrizA, vector<vector<int>> matrizB, 
 	//Programa...	
 }
 
-bool num_exp2(double num) {
+bool abre_arq(string letra, int tam, vector<vector<int>> &matriz) {
+	string nomeArq = "matrizes/"+letra+to_string(tam)+"x"+to_string(tam)+".txt";
+  	ifstream arq (nomeArq);
+  	string linha;
+  	if (!arq.is_open()) {
+  		cout << "Não foi possível abrir o arquivo" << endl;
+  		return false;
+  	}
+  	
+  	int i = 0;
+  	int subs;
+  	getline(arq, linha);
+  	while(getline(arq, linha)){
+  		istringstream iss (linha);
+  		for (int j = 0; j < tam; j++) {
+			iss >> subs;
+			matriz[i][j] = subs;
+  		}
+  		i++;
+  	}
+	return true;
+}
+
+bool num_pot2(double num) {
 	if (num <= 1) {
 		if (num == 1) { return true;}
 		return false;
 	}
-	return num_exp2(num/2);
+	return num_pot2(num/2);
 }
--- src/mult_matriz.cpp
+++ src/mult_matriz.cpp
@@ -1,19 +1,46 @@
 #include "mult_matriz.h"
-#include <sstream>
+#include <stdio.h>
 #include <iostream>
 
 int main( int argc, char * argv[] ) {
 	if ( argc > 2 ) { 
-		int tam = 8;
 		string prog;
+		int tam;
+		stringstream( argv[1] ) >> tam;
 		stringstream( argv[2] ) >> prog;
 
-		if (num_exp2(tam)) {
-			cout << "Sim" << endl;
+		if (num_pot2(tam) && tam != 2 && (prog == "S" || prog == "C")) {
+		    vector<vector<int>> matrizA (tam, vector<int>(tam));
+		    vector<vector<int>> matrizB (tam, vector<int>(tam));
+		    vector<vector<int>> matrizC (tam, vector<int>(tam));
+
+  			if (!abre_arq("A", tam, matrizA)) return 0; 
+  			if (!abre_arq("B", tam, matrizB)) return 0; 
+  			/*
+  			for (int i = 0; i < tam; ++i)
+  			{
+  				for (int j = 0; j < tam; ++j)
+  				{
+  					cout << matrizA[i][j] << " ";
+  				}
+  				cout << endl;
+  			}
+  			cout << endl;
+  			for (int i = 0; i < tam; ++i)
+  			{
+  				for (int j = 0; j < tam; ++j)
+  				{
+  					cout << matrizB[i][j] << " ";
+  				}
+  				cout << endl;
+  			}
+  			*/
+			//Programa...
 		} else {
-			cout << "Nao" << endl;
+			cout << tam << " não é um valor válido, tente inserir um número que é maior que 2 e potência de base 2" << endl;
+			cout << "Ou" << endl;
+			cout << prog << " não é uma string válida, tente inserir S para sequencial e C para concorrente" << endl;
 		}
-		//Programa...
 	} else {
 		cout <<"Parâmetros inválidos. Entrada correta seria do tipo: ./[arquivo bin] [Numero de colunas/linhas] [S para rodar o programa sequencial ou C para rodar o programa Concorrente]" << endl;
 	}
