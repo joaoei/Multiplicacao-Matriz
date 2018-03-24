9e3a1a371d25c892c805ab8a978c1f1a0e788248
Mateus Santiago Ferreira Costa <boramateus@A303-30aL.imd.local>
2018-03-24 12:13:17 -0300

    Algumas correções

--- include/mult_matriz.h
+++ include/mult_matriz.h
@@ -8,3 +8,11 @@ void mult_matriz_seq(vector<vector<int>> matrizA, vector<vector<int>> matrizB, v
 void mult_matriz_conc(vector<vector<int>> matrizA, vector<vector<int>> matrizB, vector<vector<int>> &matrizC, int tam) {
 	//Programa...	
 }
+
+bool num_exp2(double num) {
+	if (num <= 1) {
+		if (num == 1) { return true;}
+		return false;
+	}
+	return num_exp2(num/2);
+}
--- src/mult_matriz.cpp
+++ src/mult_matriz.cpp
@@ -1,9 +1,18 @@
-#include "teste.h"
-
-using namespace std;
+#include "mult_matriz.h"
+#include <sstream>
+#include <iostream>
 
 int main( int argc, char * argv[] ) {
-	if ( argc == 3 ) { 
+	if ( argc > 2 ) { 
+		int tam = 8;
+		string prog;
+		stringstream( argv[2] ) >> prog;
+
+		if (num_exp2(tam)) {
+			cout << "Sim" << endl;
+		} else {
+			cout << "Nao" << endl;
+		}
 		//Programa...
 	} else {
 		cout <<"Parâmetros inválidos. Entrada correta seria do tipo: ./[arquivo bin] [Numero de colunas/linhas] [S para rodar o programa sequencial ou C para rodar o programa Concorrente]" << endl;
