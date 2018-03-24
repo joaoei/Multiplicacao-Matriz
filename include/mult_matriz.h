5241c56dd71e4c5cc05212a9d36c8023685635c9
Mateus Santiago Ferreira Costa <boramateus@A303-30aL.imd.local>
2018-03-24 10:57:51 -0300

    Organização inicial

---
+++ include/mult_matriz.h
@@ -0,0 +1,10 @@
+using namespace std;
+#include <vector>
+
+void mult_matriz_seq(vector<vector<int>> matrizA, vector<vector<int>> matrizB, vector<vector<int>> &matrizC, int tam) {
+	//Programa...
+}
+
+void mult_matriz_conc(vector<vector<int>> matrizA, vector<vector<int>> matrizB, vector<vector<int>> &matrizC, int tam) {
+	//Programa...	
+}
---
+++ src/mult_matriz.cpp
@@ -0,0 +1,11 @@
+#include "teste.h"
+
+using namespace std;
+
+int main( int argc, char * argv[] ) {
+	if ( argc == 3 ) { 
+		//Programa...
+	} else {
+		cout <<"Parâmetros inválidos. Entrada correta seria do tipo: ./[arquivo bin] [Numero de colunas/linhas] [S para rodar o programa sequencial ou C para rodar o programa Concorrente]" << endl;
+	}
+}
